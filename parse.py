102f260653c90cd0032544731f448042a8b5047a
Noah Alderton <noahlouisalderton@gmail.com>
2019-08-21 23:32:12 -0400

    Initial commit

---
+++ parse.py
@@ -0,0 +1,24 @@
+import lxml.html as lh
+import xlsxwriter
+import re
+
+html = open("Bork_Table.html", "r")
+
+doc = lh.fromstring(html.read())
+
+speakers = doc.xpath('//td/strong')
+statements = doc.xpath('//td/p')
+
+workbook = xlsxwriter.Workbook('Bork.xlsx')
+worksheet = workbook.add_worksheet()
+
+worksheet.write('A1', 'Speaker')
+worksheet.write('B1', 'Statements')
+
+row = 1
+for speaker in speakers:
+    worksheet.write(row, 0, speaker.text_content())
+    worksheet.write(row, 1, re.sub(r'\s\s+', ' ', statements[row - 1].text_content()))
+    row += 1
+
+workbook.close()
